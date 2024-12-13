624951559848369c58c96cb4475ae2dc364495fb
karim selim <kareemsleem7@gmail.com>
2024-12-14 01:49:08 +0200

    changing the logo and fixing the footer

--- components/Home/Footer.jsx
+++ components/Home/Footer.jsx
@@ -46,46 +46,6 @@ const Footer = () => {
           ease: [0.666, 0, 0.237, 1],
         }}
       />
-      <motion.div
-        style={{
-          backgroundColor: '#1f2937',
-          color: '#9ca3af',
-          borderRadius: '0.375rem',
-          padding: isMobileView
-            ? '0.125rem 0.25rem'
-            : isTabletView
-            ? '.2rem .5rem'
-            : '0.25rem 0.5rem',
-          fontSize: isMobileView
-            ? '0.625rem'
-            : isTabletView
-            ? '0.75rem'
-            : '1rem',
-          lineHeight: isMobileView
-            ? '0.875rem'
-            : isTabletView
-            ? '1.3rem'
-            : '1.5rem',
-          zIndex: 10,
-          whiteSpace: 'nowrap',
-        }}
-        variants={variants}
-        custom={viewProps}
-        transition={{
-          duration: 0.9,
-          ease: [0.666, 0, 0.237, 1],
-        }}
-      >
-        Powered by{' '}
-        <a
-          href="https://favesco.tech"
-          target="_blank"
-          rel="noopener noreferrer"
-          style={{ color: '#d1d5db', textDecoration: 'none' }}
-        >
-          Favesco
-        </a>
-      </motion.div>
     </motion.div>
   );
 };
